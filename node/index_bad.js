cd57d1a2553f8bbf1ad9cc084368879271d486ee
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-06-28 13:40:48 +0200

    Create index_bad.js

---
+++ node/index_bad.js
@@ -0,0 +1,36 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+    let cpuCount = require('os').cpus().length;
+    for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+}
+} else {
+  var express = require('express');
+  const NoSQLClient = require('oracle-nosqldb').NoSQLClient;
+  const ServiceType = require('oracle-nosqldb').ServiceType;
+
+
+  var app = express();
+
+  app.get('/', async function (req, resW) {
+    console.log('Here!' + cluster.worker.id);
+    let statement = `SELECT * FROM blogtable LIMIT 100`;
+    const rows = [];
+
+    client = createClient();
+    try {
+      let cnt ;
+      let res;
+      do {
+         res = await client.query(statement, { continuationKey:cnt});
+         rows.push.apply(rows, res.rows);
+         cnt = res.continuationKey;
+      } while(res.continuationKey != null);
+      resW.send(rows)
+    } catch (err){
+        console.log('Err!' + cluster.worker.id);
+        console.log(err);
+    } finally {
+        console.log('Finally!' + cluster.worker.id);
