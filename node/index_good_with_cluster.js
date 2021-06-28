f0b1a9bcdb86a7bbc271de5e5372c832b44cff6e
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-06-28 16:08:30 +0200

    Create index_good_with_cluster.js

---
+++ node/index_good_with_cluster.js
@@ -0,0 +1,51 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+    let cpuCount = require('os').cpus().length;
+    for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+}
+} else {
+  var express = require('express');
+  const NoSQLClient = require('oracle-nosqldb').NoSQLClient;
+  const ServiceType = require('oracle-nosqldb').ServiceType;
+
+
+  var app = express();
+
+  app.get('/', async function (req, resW) {
+    console.log('Here!' + cluster.worker.id);
+    let statement = `SELECT * FROM blogtable LIMIT 100`;
+    const rows = [];
+
+    try {
+      let cnt ;
+      let res;
+      do {
+         res = await client.query(statement, { continuationKey:cnt});
+         rows.push.apply(rows, res.rows);
+         cnt = res.continuationKey;
+      } while(res.continuationKey != null);
+      resW.send(rows)
+    } catch (err){
+        console.log('Err!' + cluster.worker.id);
+        console.log(err);
+    } finally {
+        console.log('Finally!' + cluster.worker.id);
+    }
+  });
+  app.listen(3000);
+  client = createClient();    
+  console.log('Application running!' + cluster.worker.id);
+
+function createClient() {
+    console.log('NoSQLClient!' + cluster.worker.id);
+  return new NoSQLClient({
+            serviceType: ServiceType.KVSTORE,
+            endpoint: 'localhost:80'
+        });
+
+}
+
+}
